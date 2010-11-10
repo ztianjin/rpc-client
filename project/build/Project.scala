d72ee2583bda82ef69c9aa4fee2107394402b7a2
Glen D Sanford <glen@twitter.com>
2010-11-09 16:17:25 -0800

    Add subversion repository

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,6 +2,8 @@ import sbt._
 import com.twitter.sbt._
 
 class RpcclientProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository {
+  override def subversionRepository = Some("http://svn.local.twitter.com/maven-private")
+
   val twitterMavenRepo = "twitter.com" at "http://maven.twttr.com"
   val specs     = buildScalaVersion match {
     case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1"

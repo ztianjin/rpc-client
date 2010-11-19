e9883b6ef292708d3983b89051a0ff62bba9c818
Nick Kallen <nkallen@tallbro.local>
2010-11-18 17:33:46 -0800

    Latest version of thrift and ostrich

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,24 +1,16 @@
 import sbt._
 import com.twitter.sbt._
 
-class RpcclientProject(info: ProjectInfo) extends StandardProject(info) with SubversionRepository {
+class RpcclientProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val twitterMavenRepo = "twitter.com" at "http://maven.twttr.com"
-  val specs     = buildScalaVersion match {
-    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1"
-    case _ => "org.scala-tools.testing" %% "specs" % "1.6.5"
-  }
+  val specs     = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
   val mockito   = "org.mockito"             % "mockito-all"  % "1.8.5" % "test"
   val vscaladoc = "org.scala-tools"         % "vscaladoc"    % "1.1-md-3"
-  val xrayspecs = buildScalaVersion match {
-    case "2.7.7" => "com.twitter" % "xrayspecs" % "1.0.7"
-    case _ => "com.twitter" %% "xrayspecs" % "2.0"
-  }
+  val xrayspecs = "com.twitter"             % "xrayspecs_2.8.0" % "2.0"
   val pool      = "commons-pool"            % "commons-pool" % "1.5.4"
-  val thrift    = "thrift"                  % "libthrift"    % "0.2.0-twitter-2"
-  val ostrich = buildScalaVersion match {
-    case "2.7.7" => "com.twitter" % "ostrich" % "1.1.14"
-    case _ => "com.twitter" %% "ostrich" % "2.2.6"
-  }
+  val thrift    = "org.apache.thrift"       % "libthrift"    % "0.5.0"
+  val ostrich   = "com.twitter"             % "ostrich" % "2.3.0"
 
-  override def disableCrossPaths = false
+  override def disableCrossPaths = true
+  override def managedStyle = ManagedStyle.Maven
 }

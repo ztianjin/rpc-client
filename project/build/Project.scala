a6d52f2233f1f50c2ffb546061e8b1b08ab25b64
Matt Freels <matt@freels.name>
2011-02-16 15:25:42 -0800

    should be on ostrich 2.3.6

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -6,11 +6,11 @@ class RpcclientProject(info: ProjectInfo) extends StandardProject(info) with Sub
   val specs     = "org.scala-tools.testing" % "specs_2.8.0"  % "1.6.5" % "test"
   val mockito   = "org.mockito"             % "mockito-all"  % "1.8.5" % "test"
 
-  val vscaladoc = "org.scala-tools" % "vscaladoc"     % "1.1-md-3"
-  val util      = "com.twitter"     % "util"          % "1.6.4"
-  val pool      = "commons-pool"    % "commons-pool"  % "1.5.4"
-  val thrift    = "thrift"          % "libthrift"     % "0.5.0"
-  val ostrich   = "com.twitter"     % "ostrich_2.8.0" % "2.2.6"
+  val vscaladoc = "org.scala-tools" % "vscaladoc"    % "1.1-md-3"
+  val util      = "com.twitter"     % "util"         % "1.6.4"
+  val pool      = "commons-pool"    % "commons-pool" % "1.5.4"
+  val thrift    = "thrift"          % "libthrift"    % "0.5.0"
+  val ostrich   = "com.twitter"     % "ostrich"      % "2.3.6"
 
   override def pomExtra =
     <licenses>
